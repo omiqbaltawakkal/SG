df91010a946e3f0061d73c8cdc95138104955195
omiqbaltawakkal <tawakkal.iqbal96@gmail.com>
2015-12-09 17:45:50 +0700

    Update shortest.py

--- shortest.py
+++ shortest.py
@@ -1,20 +1,54 @@
-def find_all_paths(graph, start, end, path=[]):
-    path = path + [start]
-    if start == end:
-        return [path]
-    if not start in graph:
-        return []
-    paths = []
-    for node in graph[start]:
-        if node not in path:
-            newpaths = find_all_paths(graph, node, end, path)
-            for newpath in newpaths:
-                paths.append(newpath)
-    return paths
-graph = {'A': ['B', 'C'],
-             'B': ['C', 'D'],
-             'C': ['D'],
-             'D': ['C'],
-             'E': ['F'],
-             'F': ['C']}
-print find_all_paths(graph,'A','D')
+class Graph:
+  def __init__(self):
+    self.nodes = set()
+    self.edges = defaultdict(list)
+    self.distances = {}
+    return self
+ 
+  def add_node(self, value):
+    self.nodes.add(value)
+    return self
+ 
+  def add_edge(self, from_node, to_node, distance):
+    self.edges[from_node].append(to_node)
+    self.edges[to_node].append(from_node)
+    self.distances[(from_node, to_node)] = distance
+    return self
+ 
+ 
+def dijsktra(graph, initial):
+  visited = {initial: 0}
+  path = {}
+ 
+  nodes = set(graph.nodes)
+ 
+  while nodes: 
+    min_node = None
+    for node in nodes:
+      if node in visited:
+        if min_node is None:
+          min_node = node
+        elif visited[node] < visited[min_node]:
+          min_node = node
+ 
+    if min_node is None:
+      break
+ 
+    nodes.remove(min_node)
+    current_weight = visited[min_node]
+ 
+    for edge in graph.edges[min_node]:
+      try:
+        weight = current_weight + graph.distance[(min_node, edge)]
+      except:
+        continue
+      if edge not in visited or weight < visited[edge]:
+        visited[edge] = weight
+        path[edge] = min_node
+ 
+  return visited, path
+test_graph = {'A':{'B':1,'C':3},
+              'B':{'A':1,'C':1},
+              'C':{'A':3,'B':1},
+             }
+print dijsktra(test_graph,'A','C')
